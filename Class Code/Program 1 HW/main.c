be32525c530cf63f56bec4be11a452e2e594397e
John Akujobi <akujobi365@gmail.com>
2022-09-08 10:42:47 -0500

    First Commit of original files

---
+++ Class Code/Program 1 HW/main.c
@@ -0,0 +1,19 @@
+/* The greeting program.  This program demonstrates some components of a simple C program.
+   Written by:  John Akujobi
+   Date:  23rd of August, 2022
+   Class:  CSC 150
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+/*
+I am resubmitting this code as part of the program 1 assignment due today.
+It was first submitted on the 23rd, but didn't run because of the "" characters.
+*/
---
+++ Class Code/hello_JCA.c
@@ -0,0 +1,13 @@
+/* The greeting program.  This program demonstrates some components of a simple C program.
+   Written by:  John Akujobi
+   Date:  23rd of August, 2022
+   Class:  CSC 150
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    printf("Hello World!\n");
+    return 0;
+}
