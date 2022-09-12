67ec74ec4b3e20e4f04b25832b3c6bf00df8baa7
John Akujobi <akujobi365@gmail.com>
2022-09-11 23:36:44 -0500

    Worked on the flowchart of the program and completed the c source file

--- Homework/Program 2/Watertank_fill_time_calculator - John Akujobi.c
+++ Homework/Program 2/Watertank_fill_time_calculator - John Akujobi.c
@@ -17,7 +17,7 @@ int main (void)
 //define the variables
     float average_hose_fill_rate, tank_height, tank_diameter, tank_radius;
     float tank_volume_ft, tank_volume_gallons, tank_fill_time;
-    int  tank_fill_time_hrs, tank_fill_time_minutes_left, tank_fill_time_mins, tank_fill_time_days_left, tank_fill_time_days, tank_fill_time_hrs_left;
+    int  tank_fill_time_hrs, tank_fill_time_minutes_left, tank_fill_time_mins;
 
 //ask the user for the height and diameter of the tank
     printf("What is the height of your tank in feet? \n");
@@ -54,24 +54,17 @@ tank_fill_time_hrs are the number of hours it takes to fill the tank
 tank_fill_time_minutes_left are the number of minutes left after the hours are calculated
 */
     tank_fill_time_hrs = tank_fill_time / 60;
-    tank_fill_time_minutes_left = ((tank_fill_time_hrs*60) - ((int)(tank_fill_time_mins) % 60))%60; 
-
-//Convers the hours left to days
-    tank_fill_time_days = tank_fill_time_hrs / 24;
-    tank_fill_time_days_left = tank_fill_time_hrs % 24;
-
-  tank_fill_time_hrs_left = ((tank_fill_time_days*24) - ((int)(tank_fill_time_hrs) % 24))%24;   
-
+ //   tank_fill_time_minutes_left = ((tank_fill_time_hrs*60) - ((int)(tank_fill_time_mins) / 60))%60; 
+    tank_fill_time_minutes_left = (tank_fill_time_mins)%60; 
 
 //print the volume of the users tank in both cubic feet and gallons
     printf("The volume of your tank is %f cubic feet \n", tank_volume_ft);
     printf("It can hold %f gallons of water \n", tank_volume_gallons);
     printf("\n");
-    printf("\n");
 
 //print the time it takes to fill the tank by returning the variable tank_fill_time_hrs to the user
     printf("It would take %f minutes to fill the tank. \n", tank_fill_time);
-    printf("That is %d days, %d hours and %d minutes. \n", tank_fill_time_days, tank_fill_time_hrs_left, tank_fill_time_minutes_left);
+    printf("That's about %d hour(s), and %d minutes. \n", tank_fill_time_hrs, tank_fill_time_minutes_left);
     printf("\n");
     return (0);
 }
