e97aadff79678abacb2737958cc0c5a60eb697e6
akujobi365 <akujobi365@gmail.com>
2022-09-28 16:59:11 -0500

    Completed Homeword Program 3 and Began Temperature conversion program

--- Homework/Program 3/Program 3 code/Hobo_Day_Mug_Calculation_John_Akujobi.c
+++ Homework/Program 3/Program 3 code/Hobo_Day_Mug_Calculation_John_Akujobi.c
@@ -11,6 +11,7 @@
 6. Print the diameter and radius of the cup to the screen
 7. Print the volume of the mug in inches cube, ounces and litres
 */
+
 //Created by John Akujobi on 9/26/2022
 //CSC 151 - S01 - SDState
 
@@ -25,94 +26,143 @@
 //Define PI
 const float PI = 3.1416;
 
-//This constant helps with converting from inches cube to ounces
+//This constant helps with converting volume from inches cube to ounces
 const float ToOunces = 0.554113;
 
-//This constant helps with converting from ounces to litres
+//This constant helps with converting volume from ounces to litres
 const float ToLitres = 0.02841306;
 
 
 
 //Define all the function Prototypes
-// This function prints a welcome message to the screen
+//This function prints a welcome message to the screen telling the user about the program
 void printWelcome ();
 
-float getDiameter ();        // reads the diameter from the users input
-float calculateArea (float c_radius);     // calculates the area of the circle, then returns the value
-float calculateCircumference (float c_radius);     // calculates the circumference of the circle, then returns the value
-void printResults (float a, float); // prints the results of the area calculation and circumference calculation
+//This function reads the diameter from the user's input then returns the value
+float getDiameter ();
 
+//Calculates the radius from the diameter, then returns the value
+float calcRadius (float diameter);
+
+//Calculate the volume in inch cubes using the hemisphere volume formula and radius, then return the value
+float calcVolume_inch (float radius);
+
+//Convert the volume from inch cubes to ounces by multiplying by ToOunces
+float calcVolume_ounces (float vol_Inches);
+
+//Convert the volume from inch ounces to litres by multiplying by ToLitres
+float calcVolume_lit (float vol_Ounces);
+
+//print the diameter and radius, and the volume in inch cubes, ounces and litres
+void printResults (float diameter, float radius, float vol_Ounces, float vol_Inches, float vol_Lit);
+//Just learnt the hard way that the order of the variables in the function call matters
+
+
+
+//The Main
 int main(void)
 {
     //declare variables
-    float diameter, volOunces, volLit, radius;
+    float diameter, vol_Inches, vol_Ounces, vol_Lit, radius;
 
 
     // print welcome message
     printWelcome();
 
-    // read radius from user and print to screen
+    // read diameter from user and print to screen
     diameter = getDiameter();
-    printf("diameter: %f\n", diameter);
+   // printf("diameter: %f\n", diameter);
 
-    // calculate the area of circle and print to screen
-    area = calculateArea(radius);
-    printf("area: %f\n", area);
+    // calculate radius from diameter
+    radius = calcRadius(diameter);
+    //printf("radius: %f\n", radius);
 
-    // calculate the circumference of the circle and print to screen
-    circumference = calculateCircumference(radius);
-    printf("circumference: %f\n", circumference);
+    // calculate volume in inches cube
+    vol_Inches = calcVolume_inch(radius);
+    //printf("volume in inches cube: %f\n", vol_Inches);
 
+    // convert volume from inches cube to ounces
+    vol_Ounces = calcVolume_ounces(vol_Inches);
+    //printf("volume in ounces: %f\n", vol_Ounces);
+
+    // convert volume from ounces to litres
+    vol_Lit = calcVolume_lit(vol_Ounces);
+    //printf("volume in litres: %f\n", vol_Lit);
+
+    // print results to the user
+    printResults(diameter, radius, vol_Inches, vol_Ounces, vol_Lit);
+    
     return 0;
 }
+
 
 
 // prints welcome message to the screen
 void printWelcome ()
 {
     printf("\n\n\n");
-    printf("Hello Fellow Jackrabbit! \n Welcome to the Hobo Day Mug program.\n;
-    printf("It calculates how much beverage can be held in your hemisphere shaped mug can\n");");
-    printf("\n\n");
+    printf("Hello Fellow Jackrabbit! \n Welcome to the Hobo Day Mug program.\n");
+    printf("It calculates how much beverage can be held in your hemisphere shaped mug can\n");
+    printf("\n");
     return;
 }
 
-
-/* readRadius:
- * It reads the radius from the user's input then returns the value
- * The value
- * returns the radius
- */
-float readRadius ()
+//This function reads the diameter of the mug from the user's input then returns the value
+float getDiameter ()
 {
-    float r_radius;
-    printf("Enter the radius of the circle of the circle: ");
-    scanf("%f", &r_radius);
-    return r_radius;
+    float diameter;
+    printf("Type in the diameter of your Hobo Day Mug --->: ");
+    scanf("%f", &diameter);
+    return diameter;
 }
 
-// calculates the area of the circle, then returns the value
-float calculateArea (float c_radius)
+//Calculates the radius from the diameter, then returns the value
+float calcRadius (float diameter)
 {
-    float c_area;
-//   c_area = PI * c_radius * c_radius;
-    c_area = PI * pow(c_radius, 2);
-    return c_area;
+    float radius = diameter/2.0;
+    return radius;
 }
 
-// calculates the circumference of the circle, then returns the value
-float calculateCircumference (float c_radius)
+//Calculate the volume in inch cubes using the hemisphere volume formula and radius, then return the value
+float calcVolume_inch (float radius)
 {
-    float c_circumference;
-    c_circumference = 2.0 * PI * c_radius;
-    return c_circumference;
+    float vol_Inches = ((2.0/3.0)* PI * pow(radius, 3));
+    return vol_Inches;
 }
 
-//print the area and circumference of the circle
-void printArea (float c_radius)
+//Convert the volume from inch cubes to ounces by multiplying by ToOunces
+float calcVolume_ounces (float vol_Inches)
 {
-    printf("area:                %8.2f", calculateArea);
-    printf("circumference:       %8.2f", calculateCircumference);
+    float vol_Ounces = vol_Inches * ToOunces;
+    return vol_Ounces;
+}
+
+//Convert the volume from inch ounces to litres by multiplying by ToLitres
+float calcVolume_lit (float vol_Ounces)
+{
+    float vol_Litres = vol_Ounces * ToLitres;
+    return vol_Litres;
+}
+
+//print the diameter and radius, and the volume in inch cubes, ounces and litres
+//and ends the program with a statement
+void printResults (float diameter, float radius, float vol_Inches, float vol_Ounces, float vol_Lit)
+{
     printf("\n");
+    printf("************************************************************\n");
+//    printf("\n");
+    printf("Your mug has a diameter of:     %9.3f\n", diameter);
+    printf("And a radius of:                %9.3f\n", radius);
+    printf("\n");
+    printf("The volume of your Hobo Day mug is \n");
+    printf("In Inches cube:                 %9.3f inches cube\n", vol_Inches);
+    printf("In Ounces:                      %9.3f ounces\n", vol_Ounces);
+    printf("And in Litres:                  %9.3f litres\n", vol_Lit);
+    printf("\n");
+    printf("************************************************************\n");
+
+    //Ending statement
+    printf("Thank you for using this program\n");
+    printf("Go Jacks!!!\n\n");
     return;
 }
