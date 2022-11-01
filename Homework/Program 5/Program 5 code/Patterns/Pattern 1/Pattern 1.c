1cb55d7d36734e8faed1d0b5f805d6f818ae2027
akujobi365 <akujobi365@gmail.com>
2022-11-01 00:28:25 -0500

    Added Pattern 4
    Figured out the code to print pattern 4

--- Homework/Program 5/Program 5 code/Patterns/Pattern 1/Pattern 1.c
+++ Homework/Program 5/Program 5 code/Patterns/Pattern 1/Pattern 1.c
@@ -1,14 +1,7 @@
 /*
 This program prints patterns using asterisks and a number based on the user's input.
 
-Pattern 1	Pattern 2	Pattern 3	Pattern 4
-* 7 7 7 7 7 7	* * * * * * *	7 7 7 7 7 7 7	* * * * * * 7
-7 * 7 7 7 7 7	* 7 7 7 7 7 *	7 * * * * * 7	* * * * * 7 *
-7 7 * 7 7 7 7	* 7 7 7 7 7 *	7 * * * * * 7	* * * * 7 * *
-7 7 7 * 7 7 7	* 7 7 7 7 7 *	7 * * * * * 7	* * * 7 * * *
-7 7 7 7 * 7 7	* 7 7 7 7 7 *	7 * * * * * 7	* * 7 * * * *
-7 7 7 7 7 * 7	* 7 7 7 7 7 *	7 * * * * * 7	* 7 * * * * *
-7 7 7 7 7 7 *	* * * * * * *	7 7 7 7 7 7 7	7 * * * * * *
+
 
 printf ("Pattern 1	    Pattern 2	    Pattern 3	    Pattern 4   \n ");
 printf ("* 7 7 7 7 7 7	* * * * * * *	7 7 7 7 7 7 7	* * * * * * 7 \n ");
@@ -21,7 +14,6 @@ printf ("7 7 7 7 7 7 *	* * * * * * *	7 7 7 7 7 7 7	7 * * * * * * \n ");
 
 */
 
-*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,29 +21,8 @@ printf ("7 7 7 7 7 7 *	* * * * * * *	7 7 7 7 7 7 7	7 * * * * * * \n ");
 int main ()
 {
     int size = 7;
+    
     int row, col;
-
-///*
-    //Pattern 4
-    int b = size;
-    for (row = 1; row <= size; row++)
-    {
-        for (col = 1; col <= size; col++)
-        {
-            if ( row == (size-row+1) || col == (size-row+1))
-            {
-                printf("%3d", size);
-            }
-            else
-            {
-                printf("  *");
-            }
-        }
-        printf("\n");
-    }
-//*/
-
-
 
 /*
     //Pattern 1
@@ -111,6 +82,28 @@ int main ()
         printf("\n");
     }
 */
+
+
+/*
+    //Pattern 4
+
+    for (row = 1; row <= size; row++)
+    {
+        for (col = 1; col <= size; col++)
+        {
+            if ( row == (size-col+1))
+            {
+                printf("%3d", size);
+            }
+            else
+            {
+                printf("  *");
+            }
+        }
+        printf("\n");
+    }
+*/
+
 
     return 0;
 }
